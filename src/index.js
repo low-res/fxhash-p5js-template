b72ff09e9b7c31eaaa6973c6632fa2f776cfdaa7
paul_webyte <ap@webyte.org>
2021-12-29 23:17:28 +0100

    include dat.gui

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import p5 from "p5";
-
+import * as dat from 'dat.gui';
 
 // these are the variables you can use as inputs to your algorithms
 console.log(fxhash)   // the 64 chars hex number fed to your algorithm
@@ -15,6 +15,10 @@ window.$fxhashFeatures = {}
 let sketch = function (p5) {
     let brush;
 
+    const props = {
+        someProp: 100
+    };
+
     p5.setup = function () {
         p5.createCanvas(WIDTH, WIDTH); // Size must be the first statement
         p5.frameRate(30);
@@ -24,6 +28,11 @@ let sketch = function (p5) {
     p5.draw = function () {
         p5.background("#f1f1e6");
     };
+
+    function createGUI() {
+        const gui = new dat.GUI;
+        gui.add(props, 'someProp', 0, 200, 1);
+    }
 
 }
 
